37f789c93fcb5a2eb72b3d8483fb3790bf6d9e6d
Anthony Brayan <brayan.farronan@gmail.com>
2025-02-14 17:34:43 +0100

    Commit 06: retos_variables 01

---
+++ clase_01/04_retos_variables.py
@@ -0,0 +1,29 @@
+#Ejercicio de variables en Python 🐍
+
+##Ejercicio 01: Información Personal
+
+"""
+
+Crear variables para almacenar:
+- Nombre
+- Edad
+- Tu altura en metros
+- Si eres estudiante o no
+
+Muestra toda la información en pantalla con un formato agradable.
+
+"""
+
+nombre = "Anthony Brayan"
+edad = 29
+altura = 1.60 
+estudiante = False
+
+print(f"Nombre: {nombre},\nEdad: {edad},\nAltura: {altura},\n¿Eres estudiante?:{estudiante}")
+
+##Ejercicio 02: Intercambio de variables.
+"Crea dos variables."
+a = 5
+b= 10
+
+"Intercambia las variables sin usar una tercera variable"
--- clase_01/reto01.py
+++
@@ -1,6 +0,0 @@
-nombre = "Anthony Brayan"
-apellido = "Farroñan Carranza"
-altura= 1.60
-estudiante= False
-
-print(f"Nombre: {nombre}, Apellido: {apellido}, altura: {altura}, estudiante: {estudiante}")
